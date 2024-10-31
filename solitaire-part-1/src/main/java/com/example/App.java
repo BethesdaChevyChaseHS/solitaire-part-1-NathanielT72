52824a1c5b8c78f157010a154fa0d6029e1f1a82
NathanielT72 <nathaniel.tessema10@icloud.com>
2024-10-31 16:55:55 +0000

    Sumbitting my code

--- solitaire-part-1/src/main/java/com/example/App.java
+++ solitaire-part-1/src/main/java/com/example/App.java
@@ -1,14 +1,45 @@
 package com.example;
 
 import javax.swing.*;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
 public class App {
     public static void main(String[] args) { 
-      //add your code here!
+      JFrame mainFrame = new JFrame();
+      mainFrame.setLayout(new BorderLayout());
+      mainFrame.setSize(600,400);
+      mainFrame.setVisible(true);
 
+      JPanel northPanel = new JPanel();
+      mainFrame.add(northPanel,BorderLayout.NORTH);
+      JLabel nathanielSolitare = new JLabel("Nathaniel's Solitare");
+      northPanel.add(nathanielSolitare);
+
+      JPanel westPanel = new JPanel();
+      mainFrame.add(westPanel,BorderLayout.WEST);
+      JLabel Pile = new JLabel("Pile");
+      westPanel.add(Pile);
+
+      JPanel playArea = new JPanel();
+      playArea.setLayout(new GridLayout(1,7));
+      for(int i = 0; i < 8; i++) {
+        JPanel Stack = new JPanel();
+        Stack.add(new JLabel("Stack " + Integer.toString(i+1)));
+        playArea.add(Stack);
+        
+      
+      }
+      mainFrame.add(playArea,BorderLayout.CENTER);
+
+      JPanel southPanel = new JPanel();
+      mainFrame.add(southPanel,BorderLayout.SOUTH);
+      JLabel southLabel = new JLabel("Thanks For Playing!");
+      southPanel.add(southLabel);
+
+
+
+
+      
 
       
     }
